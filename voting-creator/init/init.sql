edce292b664e010a724db2e10f525ef5da86b31e
rohit <rohitchakraborty808@gmail.com>
2026-03-13 02:33:47 +0530

    added containerization for creator and service, and upgraded springboot parent

---
+++ voting-creator/init/init.sql
@@ -0,0 +1,11 @@
+create table creator(creator_id varchar(10) constraint creatorpk primary key,creator_name varchar(100) not null,creator_info varchar(500));
+create table event(event_id varchar(10) constraint eventpk primary key, event_name varchar(100) not null,event_info varchar(500),creator_id varchar(10) not null, constraint eventfk1 foreign key(creator_id) references creator(creator_id));
+create table candidate(candidate_id varchar(10), candidate_name varchar(100) not null, candidate_info varchar(500),primary key(candidate_id));
+create table nomination(candidate_id varchar(10),event_id varchar(10),constraint nominationfk1 foreign key(candidate_id) references candidate(candidate_id),constraint nominationfk2 foreign key(event_id) references event(event_id));
+create sequence creator_creator_id_seq owned by creator.creator_id;
+alter table creator alter column creator_id set default nextval('creator_creator_id_seq');
+create sequence event_event_id_seq owned by event.event_id;
+alter table event alter column event_id set default nextval('event_event_id_seq');
+alter table event add column "locked" boolean not null default false;
+create sequence candidate_candidate_id_seq owned by candidate.candidate_id;
+alter table candidate alter column candidate_id set default nextval('candidate_candidate_id_seq');
